18b54b5095cd685cc97a77f51ead9a25ad11e515
ludoBcg <blache.ludovic@gmail.com>
2026-06-11 17:13:57 +0200

    add compute shader

---
+++ src/shaders/compile.bat
@@ -0,0 +1,4 @@
+C:/VulkanSDK/1.3.250.1/Bin/glslc.exe vert_shader.vert -o vert.spv
+C:/VulkanSDK/1.3.250.1/Bin/glslc.exe frag_shader.frag -o frag.spv
+C:/VulkanSDK/1.3.250.1/Bin/glslc.exe comp_shader.comp -o comp.spv
+pause
