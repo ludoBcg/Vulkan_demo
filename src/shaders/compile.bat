d52fb624d98ab09d115578069d41e3005450913d
ludoBcg <blache.ludovic@gmail.com>
2026-06-17 17:01:47 +0200

    compile script

--- src/shaders/compile.bat
+++ src/shaders/compile.bat
@@ -1,4 +1,6 @@
 C:/VulkanSDK/1.3.250.1/Bin/glslc.exe vert_shader.vert -o vert.spv
 C:/VulkanSDK/1.3.250.1/Bin/glslc.exe frag_shader.frag -o frag.spv
 C:/VulkanSDK/1.3.250.1/Bin/glslc.exe comp_shader.comp -o comp.spv
+C:/VulkanSDK/1.3.250.1/Bin/glslc.exe vert_shader_particles.vert -o vert_particles.spv
+C:/VulkanSDK/1.3.250.1/Bin/glslc.exe frag_shader_particles.frag -o frag_particles.spv
 pause
